237616779b33c3f6cc828f5d315d44097226020c
Zieyaad <zieyaadmoses@gmail.com>
2024-10-12 14:42:45 +0200

    adjusting placeholder image widths

--- app/page.tsx
+++ app/page.tsx
@@ -11,9 +11,9 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center justify-center">
-        <div className="flex items-center justify-center gap-8">
+        <div className="flex flex-col sm:flex-row items-center justify-center gap-4 smgap-8">
           <Image
-            className="dark:invert"
+            className="max-w-[120px] sm:max-w-[180px] dark:invert"
             src="https://nextjs.org/icons/next.svg"
             alt="Next.js logo"
             width={180}
@@ -27,9 +27,10 @@ export default function Home() {
             width={180}
             height={38}
             priority
+            className="max-w-[140px] sm:max-w-[180px]"
           />
         </div>
-        <h1 className="text-3xl">Website under development</h1>
+        <h1 className="text-2xl sm:text-3xl text-center">Website under development</h1>
       </main>
     </div>
   );
