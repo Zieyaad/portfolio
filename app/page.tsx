07b07a30145da1fea8330fa880bceb31e4eb08cc
Zieyaad <zieyaadmoses@gmail.com>
2024-10-12 13:09:42 +0200

    generic text - prismic

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,26 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <h1 className={'text-3xl'}>Website under development</h1>
+      <main className="flex flex-col gap-8 row-start-2 items-center justify-center">
+        <div className="flex items-center justify-center gap-8">
+          <Image
+            className="dark:invert"
+            src="https://nextjs.org/icons/next.svg"
+            alt="Next.js logo"
+            width={180}
+            height={38}
+            priority
+          />
+          <div className="text-3xl">+</div>
+          <Image
+            src="../assets/prismic.svg"
+            alt="Prismic Logo"
+            width={180}
+            height={38}
+            priority
+          />
+        </div>
+        <h1 className="text-3xl">Website under development</h1>
       </main>
     </div>
   );
