fbe8b2a8ddb1cb0d1a552fa2bc074d31eded0e12
Zieyaad <zieyaadmoses@gmail.com>
2025-01-30 22:29:32 +0200

    fixing build issue

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,9 +10,9 @@ function Hero() {
         <div className="h-9 w-9 bg-midnight-charcoal"></div>
       </div>
       <h1 className="text-center font-bold text-text">
-        Hi!
-        <br />
-        I'm Zieyaad
+        {/*Hi!*/}
+        {/*<br />*/}
+        {/*I'm Zieyaad*/}
       </h1>
     </section>
   );
